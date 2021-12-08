c19297d486679036c130563278f8fe79471b0537
Adrian Reimann <adrian.reimann@netpay.co.uk>
2021-12-08 20:28:43 +0000

    tidied up day 1 part 1  (when I needed a break from part 2..)

--- 8/8-1.php
+++ 8/8-1.php
@@ -1,10 +1,10 @@
 <?php
 
-//define constants for the unique segment counts we're after
-const SEGMENT_COUNT_1 = 2;
-const SEGMENT_COUNT_4 = 4;
-const SEGMENT_COUNT_7 = 3;
-const SEGMENT_COUNT_8 = 7;
+//define array constant for the unique segment counts we're after (count => resulting number)
+const SEGMENT_COUNT = [2 => 1,
+    4 => 4,
+    3 => 7,
+    7 => 8];
 
 //read input and format it into arrays (part before pipe are the signal patterns, part after are the output values
 $patterns = [];
@@ -17,17 +17,14 @@ while (($input_line = fgets($input)) !== false) {
 }
 fclose($input);
 
-//create array of unique counts so we can check "in_array" in the loop later
-$unique_segment_lengths = [SEGMENT_COUNT_1, SEGMENT_COUNT_4, SEGMENT_COUNT_7, SEGMENT_COUNT_8];
-
 //loop through all outputs and check if their char count matches one of the unique ones
 $unique_seg_digit_count = 0;
 foreach ($outputs as $output_row) {
     foreach ($output_row as $single_output) {
-        if(in_array(strlen($single_output), $unique_segment_lengths)) {
+        if(isset(SEGMENT_COUNT[strlen($single_output)])) {
             $unique_seg_digit_count++;
         }
     }
 }
 
-echo "The digits 1, 4, 7 or 8 appeared $unique_seg_digit_count times" . PHP_EOL;
+echo "The digits 1, 4, 7 or 8 appeared $unique_seg_digit_count times." . PHP_EOL;
