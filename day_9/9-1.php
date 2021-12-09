cf163080d4c33bcb423d7913767b1d686e6d6845
Adrian Reimann <adrian.reimann@netpay.co.uk>
2021-12-09 22:51:16 +0000

    tidied up day 9 part 1 by moving some code into a separate function in line with the part 2 solution

--- day_9/9-1.php
+++ day_9/9-1.php
@@ -11,28 +11,11 @@ fclose($input);
 
 //loop through the heightmap and check every value against values right and left and up and down (x or y, +1 or -1, for each)
 $total_risk = 0;
-foreach ($heightmap as $y_axis => $height_row) {
-    foreach ($height_row as $x_axis => $height) {
-        $lowest = true;
-        if(isset($height_row[$x_axis+1]) && $height >= $height_row[$x_axis+1]) { //check to the right
-            $lowest = false;
-            continue;
-        }
-        if(isset($height_row[$x_axis-1]) && $height >= $height_row[$x_axis-1]) { //check to the left
-            $lowest = false;
-            continue;
-        }
-        if(isset($heightmap[$y_axis+1][$x_axis]) && $height >= $heightmap[$y_axis+1][$x_axis]) { //check up
-            $lowest = false;
-            continue;
-        }
-        if(isset($heightmap[$y_axis-1][$x_axis]) && $height >= $heightmap[$y_axis-1][$x_axis]) { //check down
-            $lowest = false;
-            continue;
-        }
+foreach ($heightmap as $y_axis => $y_val) {
+    foreach ($y_val as $x_axis => $height) {
 
         //if no previous condition has set this to false, it must be a low point
-        if($lowest) {
+        if (isLowest($heightmap, $x_axis, $y_axis)) {
             $total_risk += ($height + 1); //risk level of a low point is the point + 1
         }
     }
@@ -40,3 +23,35 @@ foreach ($heightmap as $y_axis => $height_row) {
 
 //result
 echo "The total risk level of all low points is $total_risk." . PHP_EOL;
+
+
+/**
+ * Get the value of a given point on a map, then compare it in all 4 directions to check if it's the lowest
+ *
+ * @author Adrian
+ * @date_created 2021-12-09
+ *
+ * @param array $map multidimensional array reflecting a map with x and y values
+ * @param int $x_axis x location to check on array
+ * @param int $y_axis y location to check on array
+ *
+ * @return bool true if lower than all neighbours
+ */
+function isLowest($map, $x_axis, $y_axis) {
+    $height = $map[$y_axis][$x_axis];
+    if (isset($map[$y_axis][$x_axis + 1]) && $height >= $map[$y_axis][$x_axis + 1]) { //check to the right
+        return false;
+    }
+    if (isset($map[$y_axis][$x_axis - 1]) && $height >= $map[$y_axis][$x_axis - 1]) { //check to the left
+        return false;
+    }
+    if (isset($map[$y_axis + 1][$x_axis]) && $height >= $map[$y_axis + 1][$x_axis]) { //check up
+        return false;
+    }
+    if (isset($map[$y_axis - 1][$x_axis]) && $height >= $map[$y_axis - 1][$x_axis]) { //check down
+        return false;
+    }
+
+    //if no previous condition has returned false, it must be a low point
+    return true;
+}
