135fbcb81c4c642a21f94dbce0b1e2c4dbaf605c
Adrian Reimann <adrian.reimann@netpay.co.uk>
2021-12-16 15:55:21 +0000

    solution for day 16 part 2

--- day_16/16-2.php
+++ day_16/16-2.php
@@ -6,7 +6,14 @@ const BYTE_LENGTH = 4;
 const BIT_COUNT_LENGTH = 15;
 const PACKET_COUNT_LENGTH = 11;
 
+const TYPE_SUM = 0;
+const TYPE_PRODUCT = 1;
+const TYPE_MIN = 2;
+const TYPE_MAX = 3;
 const TYPE_LITERAL = 4;
+const TYPE_GT = 5;
+const TYPE_LT = 6;
+const TYPE_EQ = 7;
 
 const OP_MODE_BIT_COUNT = 0;
 const OP_MODE_PACKET_COUNT = 1;
@@ -28,10 +35,10 @@ foreach(str_split($hex_str, 1) as $part) {
 
 //recursively read the packet
 $pointer = 0;
-$ver_sum = readPacket($bit_str, $pointer);
+$value = readPacket($bit_str, $pointer);
 
 //result
-echo "The sum of all versions in this package is $ver_sum." . PHP_EOL;
+echo "The BITS transmission evaluates to $value." . PHP_EOL;
 
 
 /**
@@ -50,9 +57,9 @@ function readPacket($bin, &$pointer) {
     //get info from packet header
     list($version, $type) = readPacketHeader($bin, $pointer);
 
-    //if the packet contains a literal, we do not actually care about it - just read to increment the pointer
+    //if the packet contains a literal,the packet value is the actual value we read from it
     if($type == TYPE_LITERAL) {
-        readLiteralPacket($bin, $pointer);
+        $packet_value = readLiteralPacket($bin, $pointer);
     }
     else { //this is an operator packet
         $mode = substr($bin, $pointer++, 1);
@@ -65,9 +72,13 @@ function readPacket($bin, &$pointer) {
 
             //until we have reached the bit count, recursively call this function and pass the pointer to continue it
             $pre_parse = $pointer;
+            $packet_values = [];
             while(($pointer - $pre_parse) < $bit_count) {
-                $version += readPacket($bin, $pointer);
+                $packet_values[] = readPacket($bin, $pointer);
             }
+
+            //handle sub-packet values based on this packet's type here
+            $packet_value = calculatePacketValue($type, $packet_values);
         }
         else { //read sub-packets by given packet count
             //find how many packets there are
@@ -75,15 +86,19 @@ function readPacket($bin, &$pointer) {
             $pointer += PACKET_COUNT_LENGTH;
 
             //for every packet, recursively call this function and pass the pointer to continue it
+            $packet_values = [];
             for ($i = 0; $i< $packet_count; $i++) {
-                $version += readPacket($bin, $pointer);
+                $packet_values[] = readPacket($bin, $pointer);
             }
+
+            //handle sub-packet values based on this packet's type here
+            $packet_value = calculatePacketValue($type, $packet_values);
         }
 
     }
 
-    //return the version (or sum of versions of below packets)
-    return $version;
+    //return the packet value
+    return $packet_value;
 }
 
 /**
@@ -131,4 +146,48 @@ function readLiteralPacket($bin, &$pointer) {
     } while ($prefix != 0);
 
     return bindec($bin_number);
+}
+
+/**
+ * For a given type, handle the list of values and return the result.
+ *
+ * @author Adrian
+ * @date_created 2021-12-16
+ *
+ * @param int $type the type determining which operation to run with the values
+ * @param array $packet_values the values to run the operation determined by type on
+ *
+ * @return int the resulting value
+ */
+function calculatePacketValue($type, $packet_values) {
+
+    $value = 0; //initalise just in case we get a faulty type
+    switch ($type) {
+        case TYPE_SUM: //get some of all values
+            $value = array_sum($packet_values);
+            break;
+        case TYPE_PRODUCT: //multiply all values
+            $value = 1;
+            foreach ($packet_values as $single_value) {
+                $value *= $single_value;
+            }
+            break;
+        case TYPE_MIN: //get minimum of all values
+            $value = min($packet_values);
+            break;
+        case TYPE_MAX: //get minimum of all values
+            $value = max($packet_values);
+            break;
+        case TYPE_GT: //two values only, if first is greater than second return 1, else 0
+            $value = (int) ($packet_values[0] > $packet_values[1]);
+            break;
+        case TYPE_LT: //two values only, if first is less than second return 1, else 0
+            $value = (int) ($packet_values[0] < $packet_values[1]);
+            break;
+        case TYPE_EQ: //two values only, if they are equal return 1, else 0
+            $value = (int) ($packet_values[0] == $packet_values[1]);
+            break;
+    }
+
+    return $value;
 }
